edea01a18ba50c17f521ef7fdc99063645119f10
Raifa509 <fathimathulraifanp@gmail.com>
2025-09-19 11:01:12 +0530

    Folders created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
   return (
     <>
-      
+      <FontAwesomeIcon icon={faInstagram} />
+      <Button variant="primary">Primary</Button>
     </>
   )
 }
