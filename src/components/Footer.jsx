bf2642525996bef750e4f57434e10f46749a9fff
Raifa509 <fathimathulraifanp@gmail.com>
2025-09-22 18:40:00 +0530

    Header,footer,preloader completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full bg-slate-800 text-white p-10 h-auto">
+        <div>
+          <h5>ABOUT US</h5>
+          <p className="text-sm mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos velit sequi
+            odio eveniet illo iste a possimus nesciunt necessitatibus voluptatum
+            debitis, cupiditate in! Quae exercitationem obcaecati dignissimos?
+            Consectetur, quisquam vitae!
+          </p>
+        </div>
+
+        <div className="md:ms-30">
+          <h5>NEWSLETTER</h5>
+          <p className="text-sm mt-3">Stay updated with our latest trends</p>
+          <div className="flex items-center">
+            <input type="text" className="bg-white mt-3" />
+            <div className="bg-yellow-300 text-black mt-3 px-1">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+          </div>
+        </div>
+
+        <div className="md:ms-15">
+          <h5>FOLLOW US</h5>
+          <p className="text-sm mt-3">Let us be social</p>
+          <div className="flex items-center mt-3">
+            <FontAwesomeIcon icon={faInstagram} size="lg" className="me-1" />
+            <FontAwesomeIcon icon={faTwitter} size="lg" className="mx-1" />
+            <FontAwesomeIcon icon={faFacebook} size="lg" className="mx-1" />
+            <FontAwesomeIcon icon={faLinkedin} size="lg" className="mx-1" />
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-black text-white flex justify-center items-center py-4 px-2 text-xs md:text-sm text-center">
+        <p>
+          Copyright © 2025 All rights reserves | This website is made with by 💛
+          Raifa
+        </p>
+      </div>
+
+    </>
   )
 }
 
--- src/users/components/Header.jsx
+++ src/users/components/Header.jsx
@@ -8,7 +8,26 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <>
-     Home
+      <div className='w-full h-12 flex justify-between items-center py-1 px-4'>
+        <img src="/logo.png" alt="" width={'30px'} />
+        <h1 className='font-semibold text-xl ml-38'>BOOK STORE</h1>
+        <div className='flex justify-center items-center me-6'>
+          <FontAwesomeIcon icon={faInstagram} size="lg" className="me-2" />
+          <FontAwesomeIcon icon={faTwitter} size="lg" className="me-2" />
+          <FontAwesomeIcon icon={faFacebook} size="lg" className="me-2" />
+          <div className='rounded border px-2 py-1 ms-2'>
+            <Link to={'/login'}>
+              <FontAwesomeIcon icon={faUser} size="sm" className="me-1" />
+              Login</Link>
+          </div>
+        </div>
+      </div>
+      <div className='bg-slate-800 h-10 text-white p-2 flex justify-center items-center'>
+        <Link to={'/'} className='text-sm'>Home</Link>
+        <Link to={'/all-books'} className='ms-6 text-sm '>Books</Link>
+        <Link to={'/careers'} className='ms-6 text-sm '>Careers</Link>
+        <Link to={'/contact'} className='ms-6 text-sm '>Contact</Link>
+      </div>
     </>
   )
 }
